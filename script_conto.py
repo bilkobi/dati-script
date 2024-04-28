858941241ce0672d2a4a1ec570a5bafd82804cfc
bilkobi <160396622+bilkobi@users.noreply.github.com>
2024-04-28 14:06:32 +0200

    Add files via upload

---
+++ script_conto.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+spark=SparkSession.builder \
+          .appName('EsempioSQL') \
+          .getOrCreate()
+df=spark.read.csv('dati/conto.csv', header=True, sep=';', inferSchema=True)
+df.createOrReplaceTempView('conto')
+risultato=spark.sql("SELECT * FROM conto WHERE codice='BA'")
+risultato.write.json("query_json")
+
+df_descr=spark.createDataFrame(
+    [('AS','Accredito stipendio'),
+     ('BA','Bonifico a'),
+     ('BD','Bonifico da'),
+     ('PC','Prelievo contanti'),
+     ('PB','Pagobancomat'),
+     ('BI', 'Bolli e imposte'),
+     ('VS','Versamento')],
+     ['codice', 'descrizione']
+)
+
+df_res=spark.sql("""SELECT codice, SUM(importo) as totale
+           FROM conto
+           GROUP BY codice""") \
+      .join(df_descr, on='codice') \
+      ['descrizione', 'totale'] \
+      .orderBy('totale', ascending=False)
+df_res.write.parquet("rslt_join")
+spark.stop()
